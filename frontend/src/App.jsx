d991bd76ddf30015ce885cd0eb2c717325bd8c4c
h3fgh9e-cloud <h3fgh9e@gmail.com>
2026-05-09 00:42:19 +0900

    업로드 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ import {
 // 실행 환경에 따라 백엔드 주소를 자동으로 설정합니다.
 const isProduction = window.location.hostname.includes('onrender.com');
 const API_BASE = isProduction 
-  ? `https://transport-backend.onrender.com/api` // 사용자님의 실제 Render 백엔드 주소로 바꿔야 함
+  ? `https://report-1-q31w.onrender.com/api` 
   : `http://${window.location.hostname}:3001/api`;
 
 function App() {
